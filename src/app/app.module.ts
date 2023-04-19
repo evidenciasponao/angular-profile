f4fb547b38be008d127bac321b1b23488b2d5019
evidenciasponao <evidenciasponao@gmail.com>
2023-04-18 22:40:43 -0500

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
-import { AboutComponent } from './about/about.component';
-import { ServicesComponent } from './services/services.component';
 import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent,
-    ServicesComponent,
     ContactComponent,
   ],
   imports: [
